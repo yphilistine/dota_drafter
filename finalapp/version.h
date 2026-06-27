5ad2d6b7c2010d48fb9daa0513602b2706fd092d
Андрей Морозов <amoroz-off@mail.ru>
2026-06-28 02:48:07 +0300

    Release app v0.0.2

--- finalapp/version.h
+++ finalapp/version.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-constexpr const char* kAppVersion      = "0.0.1";
+constexpr const char* kAppVersion      = "0.0.2";
 constexpr int         kSupportedSchema = 1;
 
 // URL манифеста обновлений
--- installer/dota_draft_setup.iss
+++ installer/dota_draft_setup.iss
@@ -1,5 +1,5 @@
 ﻿#define appname "Dota_Drafter"
-#define version "0.0.1"
+#define version "0.0.2"
 #define url "https://github.com/yphilistine/dota_drafter"
 #define exename "Dota_Drafter.exe"
 
