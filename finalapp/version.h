9b9f0d60e9be6ad2a7b72268e8055f574f5e8b7c
Андрей Морозов <amoroz-off@mail.ru>
2026-07-05 22:15:35 +0300

    Release app v0.4.4

--- finalapp/version.h
+++ finalapp/version.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-constexpr const char* kAppVersion      = "0.4.3";
+constexpr const char* kAppVersion      = "0.4.4";
 constexpr int         kSupportedSchema = 1;
 
 // URL манифеста обновлений
